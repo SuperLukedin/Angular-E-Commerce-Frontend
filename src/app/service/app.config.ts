bc10773ba29c0d20a6268b8b33cf4f8c2236acd1
Yuchen Peng <yuchenpeng100@gmail.com>
2020-08-02 12:22:14 -0400

    for deploy v1

--- src/app/service/app.config.ts
+++ src/app/service/app.config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-  public static API_URL = 'http://localhost:3000';
-  public static AUTH_API_URL = 'http://localhost:3000';
+  public static API_URL = 'http://54.91.153.147:3000';
+  public static AUTH_API_URL = 'http://54.91.153.147:3000';
 }
